5996dad76c4da24a06ed3a111d2d57fbdebee94d
Tmjk <thomasandyjk@gmail.com>
2020-03-04 17:51:41 +0800

    Refactoring

--- Disease_Prevention_Project/v2/video_demo.py
+++ Disease_Prevention_Project/v2/video_demo.py
@@ -44,6 +44,31 @@ def same_face_indices(prev_encodings, prev_locations):
     return same_indices
 
 
+def draw_results(locations, results):
+    for (top, right, bottom, left), (id, min_distance) \
+            in zip(locations, results):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.rectangle(frame, (left, bottom - 70),
+                      (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(
+            frame,
+            str(f'{id}, Diff: {min_distance: > .4f}'),
+            (left + 6, bottom - 42),
+            font, 1.0, (255, 255, 255), 1)
+        cv2.putText(
+            frame,
+            str(f' {"Not Matched" if min_distance > 0.35 else "Matched"}'),
+            (left + 6, bottom - 6),
+            font, 1.0, (255, 255, 255), 1)
+
+
 if __name__ == '__main__':
     user_profile_list = get_encodings('teacher.db')
     known_face_encodings = [x[0] for x in user_profile_list]
@@ -63,36 +88,27 @@ if __name__ == '__main__':
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
         rgb_small_frame = small_frame[:, :, ::-1]
 
+        # For every two frames, Skip one frame.
         if frame_count % 2 == 0:
-            # encode the frame of camera
-            new_location = face_recognition.face_locations(rgb_small_frame)
-            if len(new_location) > 0 or (
+            new_locations = face_recognition.locations(rgb_small_frame)
+            if len(new_locations) > 0 or (
                 dt.datetime.now() - last_detected_time
             ).total_seconds() > 2.0:
-                print((
-                    dt.datetime.now() - last_detected_time
-                ).total_seconds())
-                face_locations = face_recognition.face_locations(
-                    rgb_small_frame)
+                locations = new_locations
                 last_detected_time = dt.datetime.now()
-                face_encodings = face_recognition.face_encodings(
-                    rgb_small_frame, face_locations)
+                encodings = face_recognition.face_encodings(
+                    rgb_small_frame, locations)
                 matched_ids = []
                 distances = []
-                for encoding in face_encodings:
+                for encoding in encodings:
                     id, distance = find_closest(known_face_encodings, encoding)
                     matched_ids.append(id)
                     distances.append(distance)
-                if len(prev_locations) < frame_buffer_size:
-                    prev_locations.append(face_locations)
-                    prev_encodings.append(face_encodings)
-                    prev_matched_ids.append(matched_ids)
-                    prev_distances.append(distances)
-                else:
-                    prev_locations[0:] = prev_locations[1:] + [face_locations]
-                    prev_encodings[0:] = prev_encodings[1:] + [face_encodings]
-                    prev_matched_ids[0:] = prev_matched_ids[1:] + [matched_ids]
-                    prev_distances[0:] = prev_distances[1:] + [distances]
+                start = 0 if len(prev_locations) < frame_buffer_size else 1
+                prev_locations[0:] = prev_locations[start:] + [locations]
+                prev_encodings[0:] = prev_encodings[start:] + [encodings]
+                prev_matched_ids[0:] = prev_matched_ids[start:] + [matched_ids]
+                prev_distances[0:] = prev_distances[start:] + [distances]
 
         if frame_count % frame_buffer_size == frame_buffer_size - 1:
             print(frame_count)
@@ -100,48 +116,29 @@ if __name__ == '__main__':
             # 取最符合的encoding
             indices_list = same_face_indices(prev_encodings, prev_locations)
             results = []
-            for face_encoding, face_location, indices in zip(
-                    face_encodings, face_locations, indices_list):
+            for encoding, location, indices in zip(
+                    encodings, locations, indices_list):
                 ids = [prev_matched_ids[row][col] for row, col in indices]
                 id = Counter(ids).most_common(1)[0][0]
                 distance = face_recognition.face_distance(
-                    known_face_encodings[id: id+1], face_encoding)[0]
+                    known_face_encodings[id: id+1], encoding)[0]
                 results.append(
                     (id, distance)
                 )
         frame_count += 1
 
-        # For every two frames, Skip one frame.
-        for (top, right, bottom, left), (id, min_distance) \
-                in zip(face_locations, results):
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+        draw_results(locations, results)
 
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            cv2.rectangle(frame, (left, bottom - 70),
-                          (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(
-                frame,
-                str(f'{id}, Diff: {min_distance: > .4f}'),
-                (left + 6, bottom - 42),
-                font, 1.0, (255, 255, 255), 1)
-            cv2.putText(
-                frame,
-                str(f' {"Not Matched" if min_distance > 0.35 else "Matched"}'),
-                (left + 6, bottom - 6),
-                font, 1.0, (255, 255, 255), 1)
+        for id, _ in results:
             now = datetime.now()
             # 若判斷現在時間-人物最後偵測時間大於十秒，則判斷此人離場，將此人資料寫進資料庫
+            user_id = user_profile_list[id][2]
             if (now - time_dict.setdefault(
-                user_profile_list[id][2], dt.datetime.min))\
+                user_id, dt.datetime.min))\
                     .total_seconds() > 10.0:
-                pass
                 # send to database
-            time_dict[id] = now
+                pass
+            time_dict[user_id] = now
 
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
