0cce651e1c9583bab2b683803c225929887ab182
Eri <eritup45@gmail.com>
2020-03-18 01:43:36 +0800

    Add database_utils.py

--- Disease_Prevention_Project/V7/encode.py
+++ Disease_Prevention_Project/V7/encode.py
@@ -7,6 +7,7 @@ import json
 # written by us
 from Excel import read_excel
 from utils import parse_args
+from database_utils import adapt_list, convert_list
 
 # Return
 # num_classes: how many images' class
@@ -25,36 +26,6 @@ def read_image_name(directory):
         num_classes += 1
 
     return num_classes, index, image_name
-
-# Convert np to json, in order to store in sqlite
-def adapt_list(list_of_face_encodings):
-    cvt_list_of_face_encodings = []
-    for i in range(len(list_of_face_encodings)):
-        cvt_list_of_face_encodings.append(json.dumps(list_of_face_encodings[i].tolist()))
-        # cvt_list_of_face_encodings = json.dumps(list_of_face_encodings[i].tolist())
-
-    # TODO:檢查db裡None是否被存成字串'None'
-    if len(list_of_face_encodings) == 0:
-        cvt_list_of_face_encodings = None
-    return cvt_list_of_face_encodings
-
-# Convert json to list
-def convert_list(cvt_list_of_face_encodings):
-    # my_list = []
-
-    # Can't find face
-    if cvt_list_of_face_encodings == 'None':
-        return None
-    elif cvt_list_of_face_encodings == None:
-        return None
-    else:
-        my_list = np.array(json.loads(cvt_list_of_face_encodings))
-    return my_list
-
-    # TODO:CHECK whether list
-    # for i, data in enumerate(cvt_list_of_face_encodings):
-    #     my_list.append(np.array(json.loads(data)))
-
 
 def print_database(database):
     # 連接資料庫(必寫)
