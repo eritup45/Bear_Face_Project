39c1490a5fe1d5063bfb33abc8159d3c05609578
www17010 <32409477+www17010@users.noreply.github.com>
2020-03-20 06:37:23 +0800

    still have problem

--- Disease_Prevention_Project/V7/set_camera.py
+++ Disease_Prevention_Project/V7/set_camera.py
@@ -11,33 +11,32 @@ def set_cam():
     else:
         print("setting.txt不存在。")
     for i in range(10):
-        camera_check= cv2.VideoCapture(i).isOpened()
-        if camera_check == False:
-            print("error")
-            break
+        camera_check= cv2.VideoCapture(i).isOpened()#Returns true if video capturing has been initialized already.
+        next_camera_check= cv2.VideoCapture(i+1).isOpened()
+        print('i :',i, 'check: ',camera_check)
+        print('i+1 :',i+1, 'check: ',next_camera_check)
         cap = cv2.VideoCapture(i)
         #print("check:",i, camera_check)
         while(True):
             ret, frame = cap.read()
+            if camera_check == False:
+                print("error")
             cv2.imshow('frame', frame)
             value = easygui.ynbox("Is this the video camera?")
-                #print("ans : i == ",i)
+            cap.release()
+            cv2.destroyAllWindows() 
             break
+                #print("ans : i == ",i)
         if value == True:
             fp = open("setting.txt", "a")
             fp.write(str(i))
             fp.close()
-            cap.release()
-            cv2.destroyAllWindows()
             #print("insert")
             return i 
         else:
             continue
         #if cv2.waitKey(1) & 0xFF == ord('q'):
         #   break
-
-
-        
     #print("camera",camera_check)
    
 
